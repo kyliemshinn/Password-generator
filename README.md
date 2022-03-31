328eb398f701e0f77c511b627c928cb80c2c1ad7
Kylie Shinn <kyliemshinn@gmail.com>
2022-03-30 17:45:06 -0700

    add confirm messages for all characters

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Password-generator
+
+## Description
+
+The purose of this project is to randomly and automatically generate a unique password when prompted by the browser. The browser will send an alert to generate a password. The user will then be prompted to 
+
+
+
+### Lessons Learned
+
+There is a lot of great takeaways from the project completed using Javascript. First and foremost, it is crucial to start out with a plan. Commenting through sections is crucial to understand a plan from start to finish and will help the code be more manageable. The second is to have very clear variable descriptions for their set given purposes. If they are not cledar enough, it will be hard to maintain where they are intended to be used for. Another lesson learned is to test the coded frequently to see what works and what doesn' as well as just having a visual representation of where you are at.
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,115 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
-var passwordMinLength = 8;
-var  passwordMaxLength = 28;
+
+//for generator to pull from
+var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
+var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 0];
+var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "["]
+
+
+//for confirmation of each character type ^ - this is going to be used later what called upon
+var conLowercase;
+var conUppercase;
+var conpecialCharacters;
+var conNumerics;
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password;//this will write to page
 
 }
 
-// Add event listener to generate button
+
+// Add event listener to generate button-- the function is going to wait to be called upon by the button above 
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+// I am presented with a series of prompts for password criteria
+
+//Input should be validated 
+//at least one character type should be selected
+//a password is generated that matches the selected criteria
+//How?
+function generatePassword(){
+
+};
+
+//issue prompts to user
+function issuePrompts(){
+
+  var userInput = parseInt(prompt("Generate a password! Please choose between 8 and 128 characters."));
+
+  if (!userInput) {
+    alert("Please enter a numeric value between 8 and 128.")
+  }
+  
+    userInput = parseInt(prompt("Generate a password! Please choose between 8 and 128 characters."));
+
+
+
+
+  characterLengthPrompt();
+  confirmLowercase();
+  confirmUppercase();
+  confirmNumerics();
+  confirmSpecialCharacters();
+  //asked for character type???
+}
+
+//prompted for the length
+//at least 8 characters and no more than 128 characters???
+function characterLengthPrompt(){
+}
+
+
+// confirm whether or not to include lowercase, 
+function confirmLowercase(){
+  confirm("will this contain lowercase?");
+}
+
+// confirm for uppercase 
+function confirmUppercase() {
+  confirm("Will this contain Uppercase?")
+}
+// confirm for numerics
+function confirmNumerics() {
+  confirm("Will this contain Uppercase?")
+}
+
+// confirm special characters
+function confirmSpecialCharacters() {
+  confirm("Will this contain special characters?")
+}
+
+//asked for character type
+function valideateUserChoice(){
+  //make sure that at least one character type should be selected
+}
+
+
+//If the user does not choose any of the criteria
+
+//if the user chooses 3 types of criteria
+
+//if the user chooses 2 types of critera
+
+//if the user chooses 1 of the critera
+
+
+
+
+
+
+//need a for loop statement for generating multiple types of passwords on command
+
+//for (var i =0; i < 128; i++)
